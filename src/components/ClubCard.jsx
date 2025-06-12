1fe002a0b20f17f1405ac6172b79b51895c37f58
Jack Winkler <jack.p.winkler@gmail.com>
2025-06-11 23:17:48 -0400

    initial setup with club components and header

---
+++ src/components/ClubCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ClubCard = ({ image, name, description, website }) => {
+
+  const handleClick = () => {
+    window.open(website);
+  };
+
+  return (
+    <div className="club-card">
+      <img src={image} alt={"couldn't render image"} style={{ width: "100px" }} />
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <button onClick={handleClick}>Visit Website</button>
+    </div>
+  );
+};
+
+export default ClubCard;
---
+++ src/components/Clubs.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ClubCard from "./ClubCard";
+const Clubs = () => {
+
+    const clubData = [
+        {
+            image: "https://www.hackumbc.tech/hackumbcdog2025.png",
+            name: "HackUMBC",
+            description: "UMBC's annual hackathon planning committee",
+            website: "https://www.hackumbc.tech"
+        },
+        {
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoReNSnWNxeFPRFjZraFYUb5__FYTTdUkfg&s",
+            name: "STARS Computing Corps",
+            description: "Computer science networking group",
+            website: "https://stars.umbc.edu"
+        },
+    ]
+
+
+    return ( 
+        <div>
+            {clubData.map((club, index) => (
+                <ClubCard
+                key={index}
+                image={club.image}
+                name={club.name}
+                description={club.description}
+                website={club.website}
+                />
+            ))}
+        </div>
+    ) }
+
+export default Clubs;
